ff82dfd77b90b45c374782532c97a3d984a4f817
Blaga Kirova <bl.kirova25@gmail.com>
2018-07-07 23:45:07 +0300

    no message

--- TripManagementApp/src/com/telerikacademy/Main.java
+++ TripManagementApp/src/com/telerikacademy/Main.java
@@ -1,14 +1,14 @@
 package com.telerikacademy;
 
-<<<<<<< HEAD
+
 import com.telerikacademy.models.Accomodation;
-=======
+
 import com.telerikacademy.exceptions.IncorrectInputException;
 import com.telerikacademy.menus.LoginMenu;
 import com.telerikacademy.menus.Menu;
 import com.telerikacademy.models.Application;
 import com.telerikacademy.users.User;
->>>>>>> 058c1605bfb3b17b90b7f18a6612ee39126d4cd5
+
 
 public class Main {
 
